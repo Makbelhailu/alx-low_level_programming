5a8137d604bc3cc7c4ccd76af0ef4282d337cfec
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 <mcwelson92@gmail.com>
2022-06-04 12:23:54 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(succes)
+ */
+int main(void)
+{
+	int n = 0;
+	char m = 'a';
+
+	while (n <= 9)
+	{
+		putchar(n + '0');
+		n++;
+	}
+
+	while (m <= 'f')
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+
+	return (0);
+}
