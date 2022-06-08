fa239d1f41468b5094c251aa553cfaaa9591de97
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 <mcwelson92@gmail.com>
2022-06-08 12:12:40 -0700

    c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_to_98 - entry
+ *
+ * @n: take ints
+ *
+ *Return: 0
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 99)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	if (n == 98)
+	{
+		printf("%d", n);
+	}
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			if (n == 98)
+			{
+				break;
+			}
+			n--;
+		}
+	}
+	printf("\n");
+}
