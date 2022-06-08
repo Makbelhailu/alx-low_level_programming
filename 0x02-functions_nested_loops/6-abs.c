f6b5e8fdf84b86cc8a77bb76229f2c5d9303341a
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 <mcwelson92@gmail.com>
2022-06-08 09:19:50 -0700

    c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - main point
+ *
+ * @int: return int
+ *
+ * Return: 0
+ */
+int _abs(int c)
+{
+	 c = abs(c);
+
+	return (c);
+}
