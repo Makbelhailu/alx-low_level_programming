a1fa8603a7283949916c833264c72e29e308b76a
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 <mcwelson92@gmail.com>
2022-06-08 10:58:47 -0700

    c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * add - entry point
+ *
+ * @a: take int
+ *
+ * @b: take int
+ *
+ * Return: a+b
+ */
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
