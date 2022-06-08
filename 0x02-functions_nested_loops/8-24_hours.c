0b020a38aa398c74a7bc71999598be44d7f0be93
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 <mcwelson92@gmail.com>
2022-06-08 10:45:47 -0700

    t

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * jack_bauer - entry point
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if ( a == 2 && b == 4)
+			{
+				break;
+			}
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					printf("%d%d:%d%d\n", a, b, c, d);
+				}
+			}
+		}
+	}
+}
