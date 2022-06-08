cb0f48434964ae40e6dab78f5e7c8658bd4b3888
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 <mcwelson92@gmail.com>
2022-06-08 14:27:57 -0700

    c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - he
+ *
+ * @n:take ints
+ *
+ * Return: 0
+ */
+void print_times_table(int n)
+{
+	n = 10;
+	printf("%d", n);
+}
