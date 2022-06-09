6768b6aef5763bb635093b8a68bf633af7ac4382
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 <mcwelson92@gmail.com>
2022-06-09 15:23:00 -0700

    c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_number - main
+ * @n: take ints
+ * Return: 0
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	if (n > 9)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
