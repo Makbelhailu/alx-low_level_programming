2c04fadfd24d9f25a2786de284ce1f4494ee32d4
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 <mcwelson92@gmail.com>
2022-06-09 10:49:12 -0700

    c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - main point
+ *
+ * Return:0
+ *
+ * @c: use chars
+ */
+int _isupper(int c)
+{
+	c = isupper(c);
+	return (c);
+}
