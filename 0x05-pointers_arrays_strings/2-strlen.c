bb4cc5921e4db09d2693d512c28e5b3d4a6cb658
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 <mcwelson92@gmail.com>
2022-06-13 12:46:55 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - entry point
+ * @s: pointer
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int le = strlen(s);
+
+	return (le);
+}
