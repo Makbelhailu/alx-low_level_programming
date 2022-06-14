82cfd6b40ad5114f4d6910bc8b3a3f9f2013e519
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 <mcwelson92@gmail.com>
2022-06-14 10:58:54 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * numerical - check digits
+ * @n: number
+ * Return: 0 or 1
+ */
+int numerical(unsigned int n)
+{
+	return (n >= '0' &&  n <= '9');
+}
+
+/**
+ * _atoi - convertion
+ * @s - string
+ * Return: the num
+ */
+int _atoi(char *s)
+{
+	unsigned int number, i;
+	int sign;
+
+	sign = 1;
+	number = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (numerical(s[i]))
+		{
+			number = (s[i] - 48) + number * 10;
+
+			if (s[i + 1] == ' ')
+			{
+				break;
+			}
+			else if (s[i] == '-')
+			{
+				sign *= -1;
+			}
+		}
+	}
+
+		return (number *sign);
+}
