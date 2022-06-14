fa07ac2bc40e80137b4b52bfb2ccef292ec5e110
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 <mcwelson92@gmail.com>
2022-06-14 09:59:36 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - entry
+ * @a: pointer
+ * @n: pointer
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n - 1; i++)
+	{
+		if (i > 0)
+		{
+			printf(", ");
+		}
+		printf("%c", a[i]);
+	}
+	putchar('\n');
+}
