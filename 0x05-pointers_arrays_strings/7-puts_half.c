f8a867f40914da85ed062903b68dc89edef93c86
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 <mcwelson92@gmail.com>
2022-06-13 19:58:18 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * puts_half - entry
+ * @str: pointer
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int len = strlen(str) / 2;
+
+	while (str[i] != '\0')
+	{
+		if (i > len - 1)
+		{
+			printf("%c", str[i]);
+		}
+		i++;
+	}
+	printf("\n");
+}
