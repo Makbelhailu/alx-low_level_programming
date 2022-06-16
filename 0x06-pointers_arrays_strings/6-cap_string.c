23bf53b06c89ea5ab2c83ca2945032900c060574
Makbelhailu <mcwelson92@gmail.com>
2022-06-16 11:19:59 -0700

    pointer

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - pacotalize strings
+ * @s: character
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		continue;
+		}
+		if (s[i] == ' ')
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+			continue;
+		}
+		else
+		{
+			if (s[i] >= 'A' && s[i] <= 'Z')
+			{
+				s[i] = s[i] + 32;
+			}
+		}
+	}
+		return (s);
+}
