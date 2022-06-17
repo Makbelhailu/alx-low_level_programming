6220dfcb981fca9686322bd10c2fcd1eb7bcf92b
Makbelhailu <mcwelson92@gmail.com>
2022-06-17 08:23:49 -0700

    pointer

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,37 +9,24 @@
 char *cap_string(char *s)
 {
 	int i;
+	int count = 0;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for (i = 0; s[i] != '\0'; i++)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		if (i == 0)
+		for (i = 0; i < 13; i++)
 		{
-			if (s[i] >= 'a' && s[i] <= 'z')
+			if (*(s + count) == sep_words[i])
 			{
-				s[i] = s[i] - 32;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 			}
 		}
-		if (i == 66)
-		{
-			s[i] = 'H';
-		}
-		if (s[i] == '.' || s[i] == '!')
-		{
-			++i;
-			if (s[i] >= 'a' && s[i] <= 'z')
-			{
-				s[i] = s[i] - 32;
-			}
-		}
-		if (s[i] == ' ' || s[i] == '\t')
-		{
-			s[i] = ' ';
-			++i;
-			if (s[i] >= 'a' && s[i] <= 'z')
-			{
-				s[i] = s[i] - 32;
-			}
-		}
+		count++;
 	}
 		return (s);
 }
