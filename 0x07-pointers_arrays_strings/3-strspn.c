e9c2df6e56bb1b568ff285e39e5bfba0e99d3d80
Makbelhailu <mcwelson92@gmail.com>
2022-06-21 05:29:44 -0700

    more more pointer

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - entry
+ * @s:char
+ * @accept: acceptor
+ * Return: s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
+		s++;
+	}
+	return (bytes);
+}
