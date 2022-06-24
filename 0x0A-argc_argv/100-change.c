01d8993e04d08a965873e0782101faccff3c9972
Makbelhailu <mcwelson92@gmail.com>
2022-06-24 07:09:15 -0700

    args

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,42 +5,6 @@
 #include <stdbool.h>
 
 /**
- * conv - convertor
- * @i: take int
- * Return: num
- */
-int conv(int i)
-{
-	int count = 0;
-
-	while (i != 0)
-	{
-		if (i % 10 == 9 || i % 10 == 7)
-		{
-			i -= 2;
-		}
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-		else if (i % 2 == 0)
-		{
-			if (i % 10 == 6)
-				i -= 1;
-			else
-				i -= 2;
-		}
-		else
-			i -= 1;
-
-		count++;
-	}
-	return (count);
-}
-
-/**
  * main - entry
  * @argc: num of arg
  * @argv: name
@@ -48,25 +12,30 @@ int conv(int i)
  */
 int main(int argc, char *argv[])
 {
-	int i, coin;
+	int cents, ncoins = 0;
 
-	coin = 0;
-
-	if (argc != 2)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	i = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
-	if (i < 0)
-		printf("0\n");
-
-	else
+	while (cents > 0)
 	{
-		coin = conv(i);
-		printf("%d\n", coin);
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
 	}
+	printf("%d\n", ncoins);
 	return (0);
 }
