f743fe0f15feb0fe550114707128cd275e32294a
Itassignment <itassignment321@gmail.com>
2022-06-24 18:41:20 +0300

    me

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,36 +1,44 @@
-#include <stdio.h>
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * times_table - entry point
+ * times_table - prints the 9 times table
  *
- * Return: 0
+ * Description: prints the 9 times table
+ *
+ * Return: void
  */
+
 void times_table(void)
 {
-	int sum, i, j;
+	int row, column, product, tens, ones;
 
-	for (i = 0; i <= 9; i++)
+	for (row = 0; row <= 9; row++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (column = 0; column <= 9; column++)
 		{
-			sum = i * j;
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
 
-			printf("%d", sum);
-			if (j != 9)
+			if (column == 0)
 			{
-				printf(",");
+				_putchar('0');
 			}
-			if (sum < 5 && j != 9)
+			else if (product < 10)
 			{
-				printf("  ");
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
 			}
-			if (sum >= 5 && j != 9)
+			else
 			{
-				printf(" ");
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
 			}
 		}
-		printf("\n");
+		_putchar('\n');
 	}
 }
