887ead201488b0c8fa09d8a76cf685f0441d78da
Itassignment <itassignment321@gmail.com>
2022-06-30 21:32:52 +0300

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - check malloc
+ * @b: recive int
+ * Return: i
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *i;
+
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	return(i);
+}
