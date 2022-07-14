f4d179a53aab7624755301dcaff18b35aa2f1723
Makbelhailu <mcwelson92@gmail.com>
2022-07-14 04:30:07 -0700

    me

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * list_len - return list_t len
+ * @h: pointer
+ * Return: len
+ */
+size_t list_len(const list_t *h)
+{
+	size_t len;
+
+	for (len = 0; h != NULL; len++)
+		;
+	return (len);
+}
