0d357eb6f39c6ea909ab2814f3e66ba0908ba6c7
Makbelhailu <mcwelson92@gmail.com>
2022-07-14 11:05:34 -0700

    me

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
  * before - func exe befor main
  * Rturn: no
  */
-void before (void) __attribute__ ((constructor))
+void __attribute__ ((constructor)) before()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
