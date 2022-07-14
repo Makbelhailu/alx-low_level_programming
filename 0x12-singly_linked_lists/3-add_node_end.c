0733fdf7efeb9421ed78a1c34f204f68599ae96f
Makbelhailu <mcwelson92@gmail.com>
2022-07-14 10:51:15 -0700

    me

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -26,11 +26,9 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new;
 	else
 	{
-		while ((temp->next) != NULL)
-		{
+		while (temp->next != NULL)
 			temp = temp->next;
-			temp->next = new;
-		}
+		temp->next = new;
 	}
 	return (*head);
 }
