c88988c03b0193682bb6b2367d4bdf139178443b
Makbelhailu <mcwelson92@gmail.com>
2022-07-18 07:01:13 -0700

    me

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,15 +3,17 @@
 /**
  * print_listint - print listint_t
  * @h: input
+ * Return: p
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t p;
 
-	while(h != NULL)
+	p = 0;
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		h=h->next;
+		h = h->next;
 		p++;
 	}
 	return (p);
