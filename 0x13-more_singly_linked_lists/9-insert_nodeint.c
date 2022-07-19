73eb1b1b7d18cd9bc43bc400046934138e084f8a
Makbelhailu <mcwelson92@gmail.com>
2022-07-19 08:38:52 -0700

    me

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx != 0)
 	{
-		for (i = 0; i < idx - 1 && *head != NULL; i++)
+		for (i = 0; i < idx - 1 && last != NULL; i++)
 		{
 			h = h->next;
 		}
