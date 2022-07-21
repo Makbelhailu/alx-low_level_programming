17fbb15ee847eafd9d910d9651a3bc325dcd377e
Makbelhailu <mcwelson92@gmail.com>
2022-07-21 12:27:41 -0700

    me

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - gets 3dian
+ * Rerurn: 1 if litil or 0 if big
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c;
+
+	c = (char *) &i;
+	return ((int)*c);
+}
