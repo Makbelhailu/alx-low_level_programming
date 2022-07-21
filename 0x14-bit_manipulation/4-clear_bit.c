01aba88fe426ce1ddf8d806bbaee878c90c81c9e
Makbelhailu <mcwelson92@gmail.com>
2022-07-21 11:29:09 -0700

    me

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,7 +12,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index > 63)
 		return (-1);
-	i = 0 << index;
-	*n = (*n & i);
+	i = 1 << index;
+	if (*n & i)
+		*n ^= m;
 	return (1);
 }
