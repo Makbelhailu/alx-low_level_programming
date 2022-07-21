1def3a72773c7a42ece97edd12a67266461de175
Makbelhailu <mcwelson92@gmail.com>
2022-07-21 06:41:18 -0700

    me

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 
 	d = 0;
-	for (i = 0; d[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 		;
 	for (i--, w = 1; i >= 0; i--, w *= 2)
 	{
